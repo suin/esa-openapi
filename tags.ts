cb8880caadb73661f9fc9b912d19dc3861a4c0a5
suin <suinyeze@gmail.com>
2021-04-22 10:04:50 +0900

    feat: 🎸 タグの説明がより詳しくなりました。
    また、ReDoc固有のx-displayNameを設定したことで、ReDocでのメニューが日本語フレンドリーになりました。
    
    x-displayNameについて: https://github.com/Redocly/redoc/blob/master/docs/redoc-vendor-extensions.md#x-displayname

--- tags.ts
+++ tags.ts
@@ -1,23 +1,58 @@
 import { TagObject } from "openapi3-ts";
 
-const tags = {
-  category: { description: "記事のカテゴリ" },
-  comment: { description: "記事のコメント" },
-  emoji: { description: "絵文字" },
-  invitation: { description: "招待" },
-  member: { description: "メンバー" },
-  post: { description: "記事" },
-  star: { description: "Star" },
-  team: { description: "チーム" },
-  user: { description: "認証中のユーザー" },
-  watch: { description: "Watch" },
-} as const;
+const tags = $tags({
+  category: {
+    description: "記事のカテゴリの管理を行うAPIです。",
+    "x-displayName": "カテゴリ",
+  },
+  comment: {
+    description: "記事のコメントの投稿や取得などを行うAPIです。",
+    "x-displayName": "コメント",
+  },
+  emoji: {
+    description: "絵文字の管理を行うAPIです。",
+    "x-displayName": "絵文字",
+  },
+  invitation: {
+    description: "メンバーの招待を行うAPIです。",
+    "x-displayName": "招待",
+  },
+  member: {
+    description: "メンバーの管理を行うAPIです。",
+    "x-displayName": "メンバー",
+  },
+  post: {
+    description: "記事の投稿や取得などを行うAPIです。",
+    "x-displayName": "記事",
+  },
+  star: {
+    description: "記事やコメントのStarに関するAPIです。",
+    "x-displayName": "Star",
+  },
+  team: {
+    description: "所属するチームに関するAPIです。",
+    "x-displayName": "チーム",
+  },
+  user: {
+    description: "認証中のユーザーを参照するAPIです。",
+    "x-displayName": "ユーザー",
+  },
+  watch: {
+    description: "記事のWatchに関するAPIです。",
+    "x-displayName": "Watch",
+  },
+} as const);
 
 type Tags = {
   readonly [K: string]: {
     readonly description: string;
+    readonly ["x-displayName"]: string;
   };
 };
+
+function $tags<T extends Tags>(tags: T): T {
+  return tags;
+}
 
 function toTagObjects(tags: Tags): ReadonlyArray<TagObject> {
   return Object.entries(tags).map(([name, value]) => ({
